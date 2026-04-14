f7d4b72d3aecfb92b487c33fb5589468052c83eb
Renato Roberto <renato.roberto.ajunior@gmail.com>
2026-04-13 21:47:45 -0300

    Implementação da lógica do item, e importação do item para o index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import createItem from "./services/item.js";
+
 const cart = [];
+console. log("Bem vindo ao carrinho da Shopee");
+
+const item1 = await createItem("fone-bluetooth", 50.55, 1);
+const item2 = await createItem("teclado mecânico", 157.73, 4);
+
+console.log(item2.subtotal());
