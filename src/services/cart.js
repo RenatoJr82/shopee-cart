11489631e7cd2528e656552550cd3042132d8a7d
Renato Roberto <renato.roberto.ajunior@gmail.com>
2026-04-13 21:25:51 -0300

    Construção do projeto, criação das postas e arquivos e acrescentado as funções do carrinho (cart.js).

---
+++ src/services/cart.js
@@ -0,0 +1,22 @@
+//Quais ações meu carrnho pode fazer
+
+//CASOS DE USO
+// adicionar item no carrinho
+async function addItem(userCart) {
+    
+}
+
+// deletar item do carrinho
+async function deleteItem(userCart, name) {
+    
+}
+
+// remover um item - diminui a quantidade de itens
+async function removeItem(userCart, index) {
+    
+}
+
+// calcular o total de itens
+async function calculateTotal(userCart) {
+    
+}
