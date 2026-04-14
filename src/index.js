202ffd120d255c59351d0a9931ce2bba6695d1f7
Renato Roberto <renato.roberto.ajunior@gmail.com>
2026-04-14 20:56:10 -0300

    Criação da lógica para a soma e adicionar item ao carrinho.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
 const cart = [];
+const myWishlist = [];
 console. log("Bem vindo ao carrinho da Shopee");
 
 const item1 = await createItem("fone-bluetooth", 50.55, 1);
 const item2 = await createItem("teclado mecânico", 157.73, 4);
 
-console.log(item2.subtotal());
+await cartService.addItem(cart, item1);
+await cartService.addItem(myWishlist, item2);
+
+console.log("Shopee Cart TOTAL IS:");
+await cartService.calculateTotal(cart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -2,8 +2,8 @@
 
 //CASOS DE USO
 // adicionar item no carrinho
-async function addItem(userCart) {
-    
+async function addItem(userCart, item) {
+    userCart.push(item);
 }
 
 // deletar item do carrinho
@@ -18,5 +18,12 @@ async function removeItem(userCart, index) {
 
 // calcular o total de itens
 async function calculateTotal(userCart) {
-    
+    console.log (userCart.reduce((total, item) => total + item.subtotal(), 0));
+}
+
+export {
+    addItem,
+    calculateTotal,
+    deleteItem,
+    removeItem
 }
