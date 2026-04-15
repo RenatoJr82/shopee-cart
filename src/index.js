7a1991a3c2978be8e344f8ee960a5d262c39c2a5
Renato Roberto <renato.roberto.ajunior@gmail.com>
2026-04-14 21:10:00 -0300

    Implementando a lógica para deletar item ao carrinho de compras

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ console. log("Bem vindo ao carrinho da Shopee");
 const item1 = await createItem("fone-bluetooth", 50.55, 1);
 const item2 = await createItem("teclado mecânico", 157.73, 4);
 
+//adicionando item
 await cartService.addItem(cart, item1);
-await cartService.addItem(myWishlist, item2);
+await cartService.addItem(cart, item2);
+
+//deletando item 
+await cartService.deleteItem(cart, item2.name);
 
 console.log("Shopee Cart TOTAL IS:");
 await cartService.calculateTotal(cart);
